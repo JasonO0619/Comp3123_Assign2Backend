39636fb18774a8184e34f5bc6fb38a76f0892771
Jason <jayopoku19@gmail.com>
2024-12-01 23:22:17 -0800

    Deployment on vercel v3

--- index.js
+++ index.js
@@ -4,25 +4,24 @@ import userRoutes from './routes/user.js';
 import employeeRoutes from './routes/employee.js';
 import cors from 'cors';
 
-
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-
 app.use(express.json());
 
 app.use('/users', userRoutes);
 app.use('/employees', employeeRoutes);
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     res.send('Hello, World!');
 });
 
-
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server running on http://localhost:${PORT}`);
+connectDB()
+    .then(() => {
+        console.log('✅ Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('❌ MongoDB Connection Error:', err.message);
     });
-});
+
 export default app;
