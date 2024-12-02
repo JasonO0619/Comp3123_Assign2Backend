65aef586781c9ed0e7e3f547c0627c32f03faa8a
Jason <jayopoku19@gmail.com>
2024-12-01 23:31:52 -0800

    Deployment on vercel v4

--- index.js
+++ index.js
@@ -6,22 +6,27 @@ import cors from 'cors';
 
 const app = express();
 
+
 app.use(cors());
 app.use(express.json());
 
 app.use('/users', userRoutes);
 app.use('/employees', employeeRoutes);
 
-app.get('/', async (req, res) => {
+
+app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-connectDB()
-    .then(() => {
-        console.log('✅ Connected to MongoDB');
-    })
-    .catch((err) => {
-        console.error('❌ MongoDB Connection Error:', err.message);
-    });
+
+(async () => {
+    try {
+        await connectDB(); 
+        console.log(' MongoDB Connected');
+    } catch (error) {
+        console.error('MongoDB Connection Error:', error.message);
+    }
+})();
+
 
 export default app;
